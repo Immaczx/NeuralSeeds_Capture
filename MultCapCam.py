22d9f528e2773445a888d0462c27f119c0da8da9
Immaczx <cpelaezg@unal.edu.co>
2021-12-03 09:18:58 -0500

    Implementacion GPIO, Funcion decofificar tiempo a segundos desde dias, horas y minutos

--- MultCapCam.py
+++ MultCapCam.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Usage: MultCapCam.py [options] <Folder> [Step] [Duration]
 
 Options:
   -h --help
   -v                 verbose mode
-  --Step <int>       Time inter each capture[default: 60]
-  --Duration <int>   Duration all capture proces[default: 300]
+  --Step <int>       Time inter each capture, use d, h, m for day, hour, minute respectli [default: 10]
+  --Duration <int>   Duration all capture proces, use d, h, m for day, hour, minute respectli [default: 50]
 """
 from docopt import docopt
 import errno
@@ -13,7 +13,15 @@ import os
 from os import path
 from picamera import PiCamera
 import time as time
+import RPi.GPIO as GPIO
 
+def code_time(time):
+    if time[-1] == 'd': timeSeg = float(time[:-1])*86400 
+    elif time[-1] == 'h': timeSeg = float(time[:-1])*3600
+    elif time[-1] == 'm': timeSeg = float(time[:-1])*60
+    else: timeSeg = float(time) 
+
+    return timeSeg
 
 #def main():
 arguments = docopt(__doc__)
@@ -26,10 +34,14 @@ except OSError as e:
     if e.errno == errno.EEXIST:
         pass
 
-Step = int(arguments['--Step']) 
-Duration = int(arguments['--Duration'])
+Step = code_time(arguments['--Step']) 
+Duration = code_time(arguments['--Duration'])
+flash = 23
 
 Final=Duration/Step
+# GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(flash, GPIO.OUT)
 
 camera = PiCamera()
 camera.start_preview(alpha=192)
@@ -37,10 +49,15 @@ camera.start_preview(alpha=192)
 cont=0
 while(cont<=Final):
     print(time.time(),time.strftime("%d-%m-%Y,%H:%M:%S"))
+    time.sleep(1)
+    GPIO.output(flash, GPIO.HIGH)
     time.sleep(2)
     camera.capture(path+"/S"+time.strftime("%d-%m-%Y,%H:%M:%S")+".jpg")
+    GPIO.output(flash, GPIO.LOW)
     camera.stop_preview()
+    time.sleep(0.2)
     cont=cont+1
+    
     time.sleep(Step-2)
 
 #if __name__ == '__main__':
